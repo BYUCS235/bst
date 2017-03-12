2235e1a3ddbc29ee12b09d0821a8e48bb4ee0c59
Mark Clement <clement@cs.byu.edu>
2017-03-11 23:25:35 -0700

    Update Test_Binary_Search_Tree_Numbers.cpp

--- Test_Binary_Search_Tree_Numbers.cpp
+++ Test_Binary_Search_Tree_Numbers.cpp
@@ -13,18 +13,21 @@ int main()
 
   Binary_Search_Tree<double> the_tree;
   double value;
+  cout << "Enter a number to add (zero to exit)"<<endl;
   while (cin >> value) {
     if (value == 0) break;
     the_tree.insert(value);
+    cout << "Preorder Traversal"<<endl;
     pre_order_traversal(the_tree, cout, 0);
+    cout << "Enter a number to add (zero to exit)"<<endl;
   }
   cin.clear();
+  cout << "Enter a number to erase (zero to exit)"<<endl;
   while (cin >> value) {
     if (value == 0) break;
     the_tree.erase(value);
+    cout << "Preorder Traversal"<<endl;
     pre_order_traversal(the_tree, cout, 0);
+    cout << "Enter a number to erase (zero to exit)"<<endl;
   }
 }
-
-
-  
